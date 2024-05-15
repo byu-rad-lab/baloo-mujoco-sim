7e9b4e5028369a941ba48e13c1a0b1c4038503c0
Curtis <curtiscarljohnson@gmail.com>
2024-05-15 17:10:23 -0600

    2 dof adaptive control working. Also changed to torques on control signal

--- single_joint/position_adaptive_control.py
+++ single_joint/position_adaptive_control.py
@@ -16,68 +16,117 @@ class ManipulatorMRACRBF:
     def __init__(
         self,
         num_gen_coords: int,
+        numberOfRBFCenters: int,
         RBFmins: npt.NDArray,
         RBFmaxes: npt.NDArray,
-        numberOfRBFCenters: int,
+        zeta=npt.NDArray,
+        time_constant=npt.NDArray,
         Lambda=1.0,
         Gamma=1.0,
         KD=1.0,
+        ctrl_dt=0.0,
     ):
+        assert len(
+            RBFmins) == 4 * num_gen_coords  #4 bc of the [q,qd,qd_r,qdd_r]
+        assert len(RBFmaxes) == 4 * num_gen_coords
+        assert len(zeta) == num_gen_coords
+        assert len(time_constant) == num_gen_coords
+
         self.numberGenCoords = num_gen_coords
         self.N = numberOfRBFCenters
         self.RBFmins = RBFmins  # should be same length as x in Phi(x)
         self.RBFmaxes = RBFmaxes  # should be same length as x in Phi(x)
 
-        assert len(RBFmins) == 4 * num_gen_coords
-        assert len(RBFmaxes) == 4 * num_gen_coords
-
         self.theta_hat = np.zeros([numberOfRBFCenters + 1, num_gen_coords])
         self.Gamma = np.eye(numberOfRBFCenters + 1) * Gamma
         self.Lambda = np.eye(num_gen_coords) * Lambda
         self.Kd = np.eye(num_gen_coords) * KD
+        self.dt = ctrl_dt
 
-        # self.Lambda = np.diag([Lambda, Lambda, 3., 3., 2.0348971137496514, 2.0348971137496514])
-        # self.Kd = np.diag([KD, KD, 1.5, 1.5, 4.035313436917082, 4.035313436917082])
+        self._create_desired_system(zeta, time_constant, ctrl_dt)
 
-        self.dt = 0.001
+    def _create_desired_system(self, zeta, tau, dt):
+        # A_ref (2*num_gen_coords x 2*num_gen_coords)
+        # B_ref (2*num_gen_coords x num_gen_forces)
+
+        #make num_gen_coords parameters, from second order system dynamics
+        m = 1.0
+        b = 2 * m / tau
+        k = (b / (2 * zeta))**2
+
+        self.x_des = np.zeros([2 * self.numberGenCoords])
+        self.xdot_des = np.zeros([2 * self.numberGenCoords])
+
+        self.Ades_stack = np.zeros(
+            [2 * self.numberGenCoords, 2 * self.numberGenCoords])
+        self.Bdes_stack = np.zeros(
+            [2 * self.numberGenCoords, self.numberGenCoords])
+        self.Ad_des_stack = np.zeros(
+            [2 * self.numberGenCoords, 2 * self.numberGenCoords])
+        self.Bd_des_stack = np.zeros(
+            [2 * self.numberGenCoords, self.numberGenCoords])
+
+        #create Aref, Bref, Ad_ref, Bd_ref for each generalized coordiate
+        for i in range(self.numberGenCoords):
+            Ades = np.array([
+                [-b[i] / m, -k[i] / m],
+                [1, 0],
+            ])
+            Bdes = np.array([[k[i] / m], [0]])
+
+            #discrete time, see https://en.wikipedia.org/wiki/Discretization#Derivation
+            Ad_des = scipy.linalg.expm(Ades * dt)
+            Bd_des = np.matmul(
+                np.linalg.inv(Ades),
+                np.matmul(Ad_des - np.eye(Ad_des.shape[0]), Bdes))
+
+            self.Ades_stack[i * 2:i * 2 + 2, i * 2:i * 2 + 2] = Ades
+            self.Bdes_stack[i * 2:i * 2 + 2, i] = Bdes.flatten()
+            self.Ad_des_stack[i * 2:i * 2 + 2, i * 2:i * 2 + 2] = Ad_des
+            self.Bd_des_stack[i * 2:i * 2 + 2, i] = Bd_des.flatten()
+
+    def _update_desired_trajectory(self, u: npt.NDArray):
+        # x_des (2*num_gen_coords x 1)
+        # r (num_gen_coords x 1)
+        #xdot_des = [qdd, qd] * num_gen_coords
+        #x_des = [qd, q] * num_gen_coords
+        self.xdot_des = self.Ades_stack @ self.x_des + self.Bdes_stack @ u
+        self.x_des = self.Ad_des_stack @ self.x_des + self.Bd_des_stack @ u
+
+        qddot_des = self.xdot_des[::2]
+        qdot_des = self.xdot_des[1::2]
+        q_des = self.x_des[1::2]
+
+        return q_des, qdot_des, qddot_des
 
     def _calc_regressor(
         self,
         q: npt.NDArray,
         qdot: npt.NDArray,
-        qdot_r: npt.NDArray,
-        qddot_r: npt.NDArray,
+        qdot_ref: npt.NDArray,
+        qddot_ref: npt.NDArray,
     ):
         assert len(q) == self.numberGenCoords
         assert len(qdot) == self.numberGenCoords
-        assert len(qdot_r) == self.numberGenCoords
-        assert len(qddot_r) == self.numberGenCoords
+        assert len(qdot_ref) == self.numberGenCoords
+        assert len(qddot_ref) == self.numberGenCoords
 
         centers = np.linspace(self.RBFmins, self.RBFmaxes, self.N)
         dMax = np.linalg.norm(centers[1, :] - centers[0, :])
-        x = np.hstack([q, qdot, qdot_r, qddot_r])
+        x = np.hstack([q, qdot, qdot_ref, qddot_ref])
         norms = np.linalg.norm(x - centers, axis=1)
         width = self.N / dMax**2
         Phi = np.exp(-width * norms**2)
 
         return np.append(Phi, 1.0)
 
-    def _calc_s(self, qdot: npt.NDArray[np.float64],
-                qdot_r: npt.NDArray[np.float64]):
-        """
-        Sliding surface corresponding to GES tracking error dynamics
-        between q and q_des.
-        """
-        s = qdot - qdot_r
-        return s
-
     def _calc_refs(
         self,
         q: npt.NDArray[np.float64],
         qdot: npt.NDArray[np.float64],
-        q_d: npt.NDArray[np.float64],
-        qdot_d: npt.NDArray[np.float64],
-        qddot_d: npt.NDArray[np.float64],
+        q_des: npt.NDArray[np.float64],
+        qdot_des: npt.NDArray[np.float64],
+        qddot_des: npt.NDArray[np.float64],
     ) -> Tuple[npt.NDArray, npt.NDArray]:
         """
         Equations 7b,7c in Slotine paper. Note that q_r is not
@@ -96,10 +145,12 @@ class ManipulatorMRACRBF:
             tuple: (reference velocity, reference acceleration)
         """
 
-        qddot_r = qddot_d - self.Lambda.dot(qdot - qdot_d)
+        qtilde_dot = qdot - qdot_des
+        qddot_ref = qddot_des - self.Lambda.dot(qtilde_dot)
 
-        qdot_r = qdot_d - self.Lambda.dot(q - q_d)
-        return qdot_r, qddot_r
+        qtilde = q - q_des
+        qdot_ref = qdot_des - self.Lambda.dot(qtilde)
+        return qdot_ref, qddot_ref
 
     def _update_weights(self, s: npt.NDArray, Phi: npt.NDArray) -> None:
         thetaDot = -self.Gamma @ np.outer(Phi, s)
@@ -109,28 +160,23 @@ class ManipulatorMRACRBF:
         self,
         q: npt.NDArray[np.float64],
         qdot: npt.NDArray[np.float64],
-        q_d: npt.NDArray[np.float64],
-        qdot_d: npt.NDArray[np.float64],
-        qddot_d: npt.NDArray[np.float64],
+        q_des: npt.NDArray[np.float64],
+        qdot_des: npt.NDArray[np.float64] = None,
+        qddot_des: npt.NDArray[np.float64] = None,
         adapt=True,
     ) -> npt.NDArray[np.float64]:
-        """
-        External API function that should be called to calculate control input.
 
-        Args:
-            q (npt.NDArray[np.float64]): _description_
-            qdot (npt.NDArray[np.float64]): _description_
-            q_d (npt.NDArray[np.float64]): _description_
-            qdot_d (npt.NDArray[np.float64]): _description_
-            qddot_d (npt.NDArray[np.float64]): _description_
-            adapt (bool, optional): If true, weights will update in response to tracking error. Defaults to True.
+        #given qd_des and qdd_des from somewhere, I calculate a desired trajectory. A conventient
+        # choice for obtaining qd_des and qdd_des is to use a critically damped 2nd roder system.
+        #if qdot_des and qddot_des are not provided, I will use the internal trajectory generator to calculate them.
 
-        Returns:
-            npt.NDArray[np.float64]: joint torques to apply to robot
-        """
-        qdot_r, qddot_r = self._calc_refs(q, qdot, q_d, qdot_d, qddot_d)
-        s = self._calc_s(qdot, qdot_r)
-        Phi = self._calc_regressor(q, qdot, qdot_r, qddot_r)
+        if qdot_des is None or qddot_des is None:
+            q_des, qdot_des, qddot_des = self._update_desired_trajectory(q_des)
+
+        qdot_ref, qddot_ref = self._calc_refs(q, qdot, q_des, qdot_des,
+                                              qddot_des)
+        Phi = self._calc_regressor(q, qdot, qdot_ref, qddot_ref)
+        s = q - qdot_ref
 
         if adapt:
             self._update_weights(s, Phi)
@@ -151,40 +197,41 @@ if __name__ == "__main__":
         ])
         return pressures
 
-    Lambda = 150.0  # time constant of sliding surface
-    Gamma = 10  # adaptation rate
-    K = 0.05  # PD gain on s (Kd*Lambda = proportional term in control law, usually a low number 0<Kd<.5)
-    RBFmins = (np.array([-np.pi, -np.pi, -np.pi, -np.pi]) * 1.0)
-    RBFmaxes = -RBFmins
-    numRBFs = 20
-    controller = ManipulatorMRACRBF(1, RBFmins, RBFmaxes, numRBFs, Lambda,
-                                    Gamma, K)
     # Load model for simulation
     model = mujoco.MjModel.from_xml_path(
         "/home/curtis/baloo_mujoco_sim/single_joint/single_joint.xml")
     data = mujoco.MjData(model)
 
-    # Create a reference system to track, critically damped, dc gain of 1 (from k/m in B), tau of .2, x= [qdot,q].T
-    dt = model.opt.timestep
-    m = 2.0
-    b = 20.0
-    k = 50.0
-    Ades = np.array([
-        [-b / m, -k / m],
-        [1, 0],
-    ])
-    Bdes = np.array([[k / m], [0]])
-    Ad_des = scipy.linalg.expm(Ades * dt)
-    Bd_des = np.matmul(np.linalg.inv(Ades),
-                       np.matmul(Ad_des - np.eye(Ad_des.shape[0]), Bdes))
-    xdes = np.array([0, 0])
-
-    r = np.array([0.5])
+    Lambda = 75.0  # time constant of sliding surface
+    Gamma = 5  # adaptation rate
+    K = 0.1  # PD gain on s (Kd*Lambda = proportional term in control law, usually a low number 0<Kd<.5)
+    RBFmins = np.array([-100] * 8) * 1.0
+    RBFmaxes = -RBFmins
+    numRBFs = 200
+    controller = ManipulatorMRACRBF(
+        2,
+        numRBFs,
+        RBFmins,
+        RBFmaxes,
+        np.array([1, 1]),
+        np.array([.2, .2]),
+        Lambda,
+        Gamma,
+        K,
+        model.opt.timestep,
+    )
 
     plotter = JointAnglePlotter(30, model.opt.timestep)
     plotter.show()
 
     paused = False
+
+    def torque2pressures(torque):
+        # convert torque to pressures
+        p0 = 200 + torque
+        p1 = 200 - torque
+
+        return p0, p1
 
     def key_callback(keycode):
         if keycode == ord(' '):
@@ -201,23 +248,20 @@ if __name__ == "__main__":
             if not paused:
                 step_start = time.time()
 
-                xdotdes = Ades.dot(xdes) + Bdes.dot(r)
-                xdes = Ad_des.dot(xdes) + Bd_des.dot(r)
+                q = np.asarray(data.sensor("left_0").data[:2])
+                qdot = np.asarray(data.sensor("left_0").data[2:])
 
-                q = np.asarray([data.sensor("left_0").data[0]])
-                qdot = np.asarray([data.sensor("left_0").data[2]])
-
-                q_des = np.asarray([xdes[1]])
-                qd_des = np.asarray([xdotdes[1]])
-                qdd_des = np.asarray([xdotdes[0]])
+                q_des = np.asarray([1.111, -1.111])
 
                 # start = time.time()
-                tau, s, theta_hat = controller.solve_for_next_u(
-                    q, qdot, q_des, qd_des, qdd_des)
-                # print(time.time() - start)
+                tau, s, theta_hat = controller.solve_for_next_u(q, qdot, q_des)
+                print(s)
 
                 ctrl = tau / 2 * 50
-                data.ctrl[0] = ctrl
+                p0, p1 = torque2pressures(ctrl[0])
+                p2, p3 = torque2pressures(ctrl[1])
+
+                data.ctrl = np.array([p0, p1, p2, p3])
                 # print(ctrl)
 
                 # mj_step can be replaced with code that also evaluates
@@ -226,11 +270,14 @@ if __name__ == "__main__":
 
                 # Pick up changes to the physics state, apply perturbations, update options from GUI.
                 viewer.sync()
-                plotter.update(model, data, {
-                    "u_cmd": r[0],
-                    "s": s.item(),
-                    "theta_hat": theta_hat
-                })
+                plotter.update(
+                    model,
+                    data,
+                    {
+                        "u_cmd": q_des[0],
+                        # "s": s.item(),
+                        # "theta_hat": theta_hat
+                    })
 
                 # Rudimentary time keeping, will drift relative to wall clock.
                 # print(time.time() - step_start)
