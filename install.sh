646a8a0fe633e7c319aeb62eb4406f5a69f2b7b1
Curtis <curtiscarljohnson@gmail.com>
2024-09-06 12:15:11 -0600

    added feature to install to force xml model regeneration. Helps in model development.

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 pip install -r requirements.txt
-export MUJOCO_ROOT_DIR=$(pip3 show mujoco | grep Location: | cut -d ' ' -f 2)/mujoco
+export PYTHON_INSTALL_LOCATION=$(pip3 show mujoco | grep Location: | cut -d ' ' -f 2)
+export MUJOCO_ROOT_DIR="${PYTHON_INSTALL_LOCATION}/mujoco"
 pip install .
+
+# force regeneration of xml model
+rm -f "${PYTHON_INSTALL_LOCATION}/baloo_mujoco_sim/assets/"*.xml
