1a899352c8f804adf969691cc63af8decb777e9d
Curtis <curtiscarljohnson@gmail.com>
2024-10-04 10:03:59 -0600

    updating python packaging. Decided building plugins via pip was a bad idea. will do via scripting.

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from skbuild import setup
-import os
-import importlib.metadata
-from pathlib import Path as path
-import sys
+from setuptools import setup, find_packages
 
-# can't get path of mujoco package because pip puts stuff in weird /tmp folder where mucodo doesn't exist.
-
-#works, but requires path setting done in install.sh
-mujoco_path = os.environ.get('MUJOCO_ROOT_DIR')
-
-setup(
-    cmake_args=[f'-DMUJOCO_ROOT_DIR:PATH={mujoco_path}'],
-    cmake_source_dir='plugin/',
-)
+setup()
