472137fdf3bce2b442da54748d758c3f157cd42e
Curtis <curtiscarljohnson@gmail.com>
2024-10-04 11:14:13 -0600

    install now works well with cmake script.

--- install.sh
+++ install.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-pip install -r requirements.txt
-export PYTHON_INSTALL_LOCATION=$(pip3 show mujoco | grep Location: | cut -d ' ' -f 2)
-export MUJOCO_ROOT_DIR="${PYTHON_INSTALL_LOCATION}/mujoco"
-pip install .
+pip3 install -e . #to symlink, should also install mujoco as dependency
 
-# force regeneration of xml model
+# build plugins and install to mujoco python installation directory
+message="Building and installing plugins to mujoco python installation directory"
+echo $message
+
+set PYTHON_INSTALL_LOCATION=$(pip3 show mujoco | grep Location: | cut -d ' ' -f 2)
+set MUJOCO_ROOT_DIR="${PYTHON_INSTALL_LOCATION}/mujoco"
+
+echo ${MUJOCO_ROOT_DIR}
+
+rm -rf "./plugin/build"
+mkdir "./plugin/build"
+cd "./plugin/build"
+cmake .. -DMUJOCO_ROOT_DIR=${MUJOCO_ROOT_DIR} -DCMAKE_BUILD_TYPE=Release
+make install
+
+
+# # optionally download c++ version of mujoco 
+# MUJOCO_VERSION=$(python3 -c "import importlib.metadata; print(importlib.metadata.version('mujoco'))")
+# BIN_URL="https://github.com/google-deepmind/mujoco/releases/download/${MUJOCO_VERSION}/mujoco-${MUJOCO_VERSION}-linux-x86_64.tar.gz"
+
+# # Download the file
+# curl -L -o "mujoco-${VERSION}.tar.gz" "$URL"
+
+# # Extract the tar.gz file
+# tar -xzf "mujoco-${VERSION}.tar.gz"
+
+# # Optionally, clean up
+# rm "mujoco-${VERSION}.tar.gz"
+
+
+# force regeneration of xml model on install
 rm -f "${PYTHON_INSTALL_LOCATION}/baloo_mujoco_sim/assets/"*.xml
